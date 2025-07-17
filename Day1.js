01b42d564fee4d532f2a05b0479edc44d88162b1
vigneshwari2829 <vigneshwaridinesh28@gmail.com>
2025-07-17 15:46:09 +0530

    new push

---
+++ Day1.js
@@ -0,0 +1,64 @@
+console.log();
+
+// Datatype
+
+/*var a=29;
+
+//var a=30
+
+//console.log(a);*/
+
+/*let b=9;
+
+ b=28;
+
+console.log(b);*/
+
+/*const c=29;
+
+console.log(c);
+
+console.log(null);
+
+console.log("vicky");
+
+console.log(29.09);
+
+console.log(true);
+
+let e;
+
+console.log(e);*/
+
+// non primities
+
+//Array
+
+/*var a= ["vicky",false,null,0978.09];
+
+console.log(a);
+
+//object {}
+
+const obj={name : 'vicky',
+    place : "tvt",
+
+};
+console.log(obj);*/
+
+function box (){ 
+    var a=10;
+    var a=20;
+    console.log(a);
+
+    let b=30;
+    b=40;
+    console.log(b);
+    let c=50;
+    console.log(c);
+}
+box();
+
+
+
+
---
+++ operations.js
@@ -0,0 +1,67 @@
+/*let a=10;
+let a1=5;
+
+console.log(a+a1);
+console.log(a-a1);
+console.log(a*a1);
+console.log(a/a1);
+console.log(a%a1);
+console.log(a**a1);*/
+
+/*let a=10
+a++
+console.log(a);
+
+let b=20
+b--
+console.log(b);*/
+
+//Assingment operator
+
+/*let a=10;
+
+a+=20;
+
+console.log(a);
+
+let b=20;
+b-=10;
+console.log(b);
+
+let c=30;
+c*=30;
+
+console.log(c);
+
+let d=10;
+d/=5;
+
+console.log(d);
+
+let e=10;
+
+e%=10;
+
+console.log(e);
+
+let f=20;
+f**=20;
+
+console.log(f);
+
+let g=10;
+
+g++=10;
+
+console.log(g);*/
+
+let a=10;
+
+a++;
+a--;
+
+console.log(a);
+
+
+
+
